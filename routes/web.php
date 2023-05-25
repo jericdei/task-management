f17c807f044754184f9dc4a35b758f7d66ed84a2
Jeric <jericjune.logan.lhoopa@gmail.com>
2023-05-25 16:09:43 +0800

    setup breeze

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::check()
+        ? to_route('dashboard')
+        : to_route('login');
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,51 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    css: {
+        devSourcemap: true,
+    },
+    server: {
+        https: false,
+        host: true,
+        strictPort: true,
+        port: 5182,
+        hmr: {
+            host: "localhost",
+            protocol: "ws",
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString()
+                    }
+                },
+            },
+        },
+    },
 });
